f78f07f3c3f83de4126108e9a36b8656de9258a8
Artem Dmitrenko <admitrenko87@gmail.com>
2023-09-02 21:20:15 +0300

    Init

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,28 +1,33 @@
-import { Button } from '@mui/material';
+import { Button } from "@mui/material";
 
-import type { NextPage } from 'next';
+import type { NextPage } from "next";
+
+import styles from "../src/button.module.css";
 
 const Home: NextPage = () => {
-	return (
-		<main
-			style={{
-				width: '100vw',
-				height: '100vh',
-				display: 'flex',
-				justifyContent: 'center',
-				alignItems: 'center',
-			}}
-		>
-			<div>
-				<header>
-					<h1>MUI5 + Next.js 12</h1>
-				</header>
-				<section>
-					<Button variant={'contained'}>Hello MUI</Button>
-				</section>
-			</div>
-		</main>
-	);
+  return (
+    <main
+    // style={{
+    //   width: "100vw",
+    //   height: "100vh",
+    //   display: "flex",
+    //   justifyContent: "center",
+    //   alignItems: "center",
+    // }}
+    >
+      <div>
+        <header>
+          <h1>MUI5 + Next.js 12</h1>
+        </header>
+        <section>
+          {/* <Button className={styles.button} variant={"contained"}>
+            Hello MUI
+          </Button> */}
+          <Button variant={"contained"}>Hello MUI</Button>
+        </section>
+      </div>
+    </main>
+  );
 };
 
 export default Home;
